0e3e1531a3697c5f83a54c316be4a4f9e2ed035c
Pondd <pond1982a@gmail.com>
2014-04-27 12:18:59 +0700

    to string

--- CaptCha.php
+++ CaptCha.php
@@ -1,16 +1,64 @@
 <?php
 
 class CaptCha {
+	private $pattern;
 	private $firstOperant;
 	private $secondOperant;
+	private $operantor;
 
-	function getFirstOperant($firstOperant){
-		return $firstOperant;
+	private $wordArray= ["Zero","One","Two","Three"];
+
+	function __construct($pattern,$firstOperant,$operantor,$secondOperant) {
+		$this->pattern = $pattern;
+		$this->firstOperant = $firstOperant;
+		$this->operantor = $operantor;
+		$this->secondOperant = $secondOperant;
 	}
-	
-	function getSecondOperant($secondOperant){
-		return $secondOperant;
+
+	function getFirstOperant(){
+		if($this->pattern==1){
+			return $this->wordArray[$this->firstOperant];
+		} else {
+			return $this->firstOperant;
+		}
 	}
+
+	function getSecondOperant(){
+		if($this->pattern==1){
+			return $this->secondOperant;
+		} else {
+			return $this->wordArray[$this->secondOperant];
+		}
+	}
+
+	function getOperantor(){
+		if($this->operantor == "1"){
+			return "+";
+		}
+		if($this->operantor == "2"){
+			return "*";
+		}
+		if($this->operantor == "3"){
+			return "-";
+		}
+	}
+
+	function getResult(){
+		if($this->getOperantor()=="+"){
+			return ($this->firstOperant)+($this->secondOperant);
+		} 
+		if($this->getOperantor()=="*"){
+			return ($this->firstOperant)*($this->secondOperant);
+		} 
+		if($this->getOperantor()=="-"){
+			return ($this->firstOperant)-($this->secondOperant);
+		} 
+	}
+
+	function toString(){
+		return $this->getFirstOperant() . $this->getOperantor() . $this->getSecondOperant() . " = " . $this->getResult();
+	}
+
 }
 
 
--- CaptChaSpec.php
+++ CaptChaSpec.php
@@ -6,12 +6,62 @@ class CaptChaSpec extends PHPUnit_Framework_TestCase {
 	public $captCha;
 
 	function testGetFirstOperant(){
-		$this->captCha = new CaptCha();
-		$this->assertEquals("1",$this->captCha->getFirstOperant("1"));
+		$this->captCha = new CaptCha("1","1","1","1");
+		$this->assertEquals("One",$this->captCha->getFirstOperant());
 	}
 
-		function testGetSecondOperant(){
-		$this->captCha = new CaptCha();
-		$this->assertEquals("1",$this->captCha->getSecondOperant("1"));
+	function testGetSecondOperant(){
+		$this->captCha = new CaptCha("1","1","1","1");
+		$this->assertEquals("1",$this->captCha->getSecondOperant());
+	}
+	
+	function testGetOperator(){
+		$this->captCha = new CaptCha("1","1","1","1");
+		$this->assertEquals("+",$this->captCha->getOperantor());
+	}
+
+	function testPattern1GetFirstText(){
+		$this->captCha = new CaptCha("1","1","1","1");
+		$this->assertEquals("One",$this->captCha->getFirstOperant());
+	}
+
+	function testPattern2GetFirstNum(){
+		$this->captCha = new CaptCha("2","1","1","1");
+		$this->assertEquals("1",$this->captCha->getFirstOperant());
+	}
+
+	function testPattern1GetSecondNum(){
+		$this->captCha = new CaptCha("1","1","1","1");
+		$this->assertEquals("1",$this->captCha->getSecondOperant());
+	}
+
+	function testPattern2SecondOpIs2GetTwo(){
+		$this->captCha = new CaptCha("2","1","1","2");
+		$this->assertEquals("Two",$this->captCha->getSecondOperant());
+	}
+
+	function testPattern1SecondOpIs2Get2(){
+		$this->captCha = new CaptCha("1","1","1","2");
+		$this->assertEquals("2",$this->captCha->getSecondOperant());
+	}
+
+	function testPattern1FirstOpIs2Get2(){
+		$this->captCha = new CaptCha("1","2","1","2");
+		$this->assertEquals("Two",$this->captCha->getFirstOperant());
+	}
+
+	function testGetResult_1_1_1_1(){
+		$this->captCha = new CaptCha("1","1","1","1");
+		$this->assertEquals("2",$this->captCha->getResult());
+	}
+
+	function testGetResult_2_3_2_1(){
+		$this->captCha = new CaptCha("2","3","2","2");
+		$this->assertEquals("6",$this->captCha->getResult());
+	}
+
+	function testToString_2_3_2_1(){
+		$this->captCha = new CaptCha("2","3","2","2");
+		$this->assertEquals("3*Two = 6",$this->captCha->toString());
 	}
 }
